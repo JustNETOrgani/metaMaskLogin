ba5d3c653ca1cac19b1426c8274342cb3c060547
JustNETlab <justnetonline@outlook.com>
2021-04-21 17:12:11 +0800

    Hash function done

---
+++ src/assets/js/hashFunc.js
@@ -0,0 +1,9 @@
+async function getHash (data) {
+  const msgUint8 = new TextEncoder().encode(data) // encode as (utf-8) Uint8Array
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8) // hash the message
+  const hashArray = Array.from(new Uint8Array(hashBuffer)) // convert buffer to byte array
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('') // convert bytes to hex string
+  return '0x' + hashHex
+}
+
+export default getHash

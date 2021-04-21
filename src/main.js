78d13b750a54b5cf9d768169b91a567feaf83501
JustNETlab <justnetonline@outlook.com>
2021-04-21 11:44:07 +0800

    Initial commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import 'element-ui/lib/theme-chalk/index.css'
+import Web3 from 'web3'
 
 Vue.config.productionTip = false
+
+// Vue.use(ElementUI)
+Vue.use(ElementUI, Web3, { locale })
 
 new Vue({
   router,
